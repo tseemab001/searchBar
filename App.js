52566923cd3412550e6975fdae34cfa1a0a36f9a
tseemab001 <123262446+tseemab001@users.noreply.github.com>
2023-03-16 17:10:33 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, FlatList, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const data = [
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Tooba Seemab' },
+  { id: 3, name: 'Bob Smith' },
+  { id: 4, name: 'Alice Johnson' },
+];
+
+const FilterSearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleSearch = () => {
+    const filtered = data.filter((item) =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => console.log(item)}>
+      <Text style={{ padding: 10 }}>{item.name}</Text>
+    </TouchableOpacity>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flexDirection: 'row', margin: 12 }}>
+        <TextInput
+          style={{
+            flex: 1,
+            height: 60,
+            paddingHorizontal: 10,
+            borderRadius: 5,
+            borderColor: 'gray',
+            borderWidth: 1,
+          }}
+          placeholder="Search"
+          value={searchQuery}
+          onChangeText={(text) => setSearchQuery(text)}
+        />
+        <TouchableOpacity
+          style={{
+            height: 60,
+            paddingHorizontal: 10,
+            borderRadius: 5,
+            backgroundColor: 'blue',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={handleSearch}
+        >
+          <Text style={{ color: 'white' }}>Search</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={filteredData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default FilterSearchBar;
